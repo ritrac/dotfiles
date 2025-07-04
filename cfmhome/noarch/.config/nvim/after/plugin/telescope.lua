908935cc14a9cc85f6000483c37c0c84ccd99862
Clément Coussirat <174454106+ritrac@users.noreply.github.com>
2025-07-04 10:10:11 +0200

    telescope

--- cfmhome/noarch/.config/nvim/after/plugin/telescope.lua
+++ cfmhome/noarch/.config/nvim/after/plugin/telescope.lua
@@ -10,3 +10,16 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' 
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
 
+vim.keymap.set('n', '<leader>td', builtin.lsp_definitions, { desc = 'Go to definition' })
+vim.keymap.set('n', '<leader>ti', builtin.lsp_implementations, { desc = 'Go to implementations' })
+vim.keymap.set('n', '<leader>tws', builtin.lsp_workspace_symbols, { desc = 'List workspace symbols' })
+vim.keymap.set('n', '<leader>tds', builtin.lsp_document_symbols, { desc = 'List document symbols' })
+vim.keymap.set('n', '<leader>ts', builtin.treesitter, { desc = 'List workspace symbols' })
+vim.keymap.set('n', '<leader>tf', function()
+    require('telescope.builtin').lsp_dynamic_workspace_symbols({ 
+	symbols='function' ,
+	file_ignore_patterns= {'/usr/include'}
+    })
+end
+    , { desc = 'List document functions' })
+
