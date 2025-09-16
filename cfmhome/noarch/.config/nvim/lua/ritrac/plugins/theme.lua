00486a3dc8ce5cc1a3b2fb45ece205e9561c61a4
Clément Coussirat <174454106+ritrac@users.noreply.github.com>
2025-09-16 09:21:14 +0200

    clean

--- cfmhome/noarch/.config/nvim/lua/ritrac/plugins/theme.lua
+++ cfmhome/noarch/.config/nvim/lua/ritrac/plugins/theme.lua
@@ -1,9 +1,29 @@
 return {
-
-    -- theme 
-   --  { "rose-pine/neovim", config=function() vim.cmd.colorscheme "rose-pine" end},
-   --  { "folke/tokyonight.nvim", config=function() vim.cmd.colorscheme "tokyonight" end},
-    { enabled=true, "scottmckendry/cyberdream.nvim", config=function() vim.cmd.colorscheme "cyberdream" end},
-   --    { "navarasu/onedark.nvim", config=function() vim.cmd.colorscheme "onedark" end},
-
+	-- theme
+	{
+		"rose-pine/neovim",
+	},
+	{
+		"folke/tokyonight.nvim",
+	},
+	{
+		"scottmckendry/cyberdream.nvim",
+	},
+	{
+		"navarasu/onedark.nvim",
+	},
+	{
+		"catppuccin/nvim",
+	},
+	{
+		"projekt0n/github-nvim-theme",
+	},
+	{
+		"EdenEast/nightfox.nvim",
+		name = "nightfox",
+		config = function()
+			require("nightfox").setup({
+			})
+		end,
+	},
 }
