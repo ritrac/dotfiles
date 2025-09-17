0eeb9d2a7d905390906e9d8a3ff7c58a1ee770e5
Clément Coussirat <174454106+ritrac@users.noreply.github.com>
2025-09-17 14:45:43 +0200

    indent

--- cfmhome/noarch/.config/nvim/lua/ritrac/plugins/treesitter.lua
+++ cfmhome/noarch/.config/nvim/lua/ritrac/plugins/treesitter.lua
@@ -5,24 +5,26 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function ()
-	require'nvim-treesitter.configs'.setup {
+        require'nvim-treesitter.configs'.setup {
 
-	    ensure_installed = {
-		"c", "cpp", "css", "go", "asm", "bash", "cuda", "diff",
-		"gitcommit", "gitignore", "gowork", "json", "cmake",
-		"make", "perl", "pod", "python", "php", "rst", "rust",
-		"ssh_config", "tmux", "xml", "yaml", "zig",
-		"lua", "vim", "vimdoc", "query", "elixir",
-		"heex", "javascript", "html" },
-	    sync_install = false,
-	    auto_install = true,
-	    highlight = { enable = true
-		-- disable = { "c", "rust" },
-	    },
-	    indent = { enable = true },
+            ensure_installed = {
+                "c", "cpp", "css", "go", "asm", "bash", "cuda", "diff",
+                "gitcommit", "gitignore", "gowork", "json", "cmake",
+                "make", "perl", "pod", "python", "php", "rst", "rust",
+                "ssh_config", "tmux", "xml", "yaml", "zig",
+                "lua", "vim", "vimdoc", "query", "elixir",
+                "heex", "javascript", "html" },
+            sync_install = false,
+            auto_install = true,
+            highlight = { enable = true
+                -- disable = { "c", "rust" },
+            },
+            indent = { enable = true },
 
+            incremental_selection = { enable = true },
+            textobjects = { enable = true },
 
-	}
+        }
     end
 }
 
