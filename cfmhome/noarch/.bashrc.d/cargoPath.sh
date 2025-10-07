36f4ebd0b312a9ef66792f398ca6b134307b4cf2
Clément Coussirat <174454106+ritrac@users.noreply.github.com>
2025-10-07 09:32:41 +0200

    fix cargo path

--- cfmhome/noarch/.bashrc.d/cargoPath.sh
+++ cfmhome/noarch/.bashrc.d/cargoPath.sh
@@ -1,7 +1,7 @@
 
 # User specific environment
 if [ -d "$HOME/.cargo/bin" ]; then
-    if ! [[ "$PATH" =~ ".cargo/bin" ]]; then
+    if ! [[ "$PATH" =~ "$HOME/.cargo/bin" ]]; then
 	PATH="$HOME/.cargo/bin:$PATH"
     fi
 fi
