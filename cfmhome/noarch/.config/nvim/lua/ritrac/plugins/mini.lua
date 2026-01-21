9d7a764bb088ff7c4299adb69457e3ebaf31c0c8
Clément Coussirat <174454106+ritrac@users.noreply.github.com>
2026-01-21 10:41:12 +0100

    nvim mini.ai

--- cfmhome/noarch/.config/nvim/lua/ritrac/plugins/mini.lua
+++ cfmhome/noarch/.config/nvim/lua/ritrac/plugins/mini.lua
@@ -3,16 +3,17 @@ return {
     'echasnovski/mini.nvim',
     enabled = true,
     config = function()
-	local statusline = require 'mini.statusline'
-	statusline.setup { use_icons = vim.g.have_nerd_font }
-	local pair = require 'mini.pairs'
-	pair.setup()
-	local icons = require 'mini.icons'
-	icons.setup()
-	local scope = require('mini.indentscope')
-	scope.setup { draw = { animation = require("mini.indentscope").gen_animation.none() },}
-	local trail = require 'mini.trailspace'
-	trail.setup()
+        local statusline = require 'mini.statusline'
+        statusline.setup { use_icons = vim.g.have_nerd_font }
+        local pair = require 'mini.pairs'
+        pair.setup()
+        local icons = require 'mini.icons'
+        icons.setup()
+        local scope = require('mini.indentscope')
+        scope.setup { draw = { animation = require("mini.indentscope").gen_animation.none() }, }
+        local trail = require 'mini.trailspace'
+        trail.setup()
+        local ai = require 'mini.ai' -- !!! not an LLM thing
+        ai.setup()
     end
 }
-
