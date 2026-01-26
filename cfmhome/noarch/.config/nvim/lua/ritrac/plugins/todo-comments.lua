ea83741954f2d21575c9a4cf39e42140c46fffe4
Clément Coussirat <174454106+ritrac@users.noreply.github.com>
2026-01-26 08:28:09 +0100

    nvim todo-comments

---
+++ cfmhome/noarch/.config/nvim/lua/ritrac/plugins/todo-comments.lua
@@ -0,0 +1,8 @@
+return {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim"},
+    enabled = true,
+    opts = {
+        -- Defaults keywords: TODO, HACK, WARN, PERF, NOTE, TEST
+    }
+}
