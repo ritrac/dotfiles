5dbdfb7610dd3e27a7d0ac339fe172b35380d8c5
Clément Coussirat <174454106+ritrac@users.noreply.github.com>
2026-03-26 14:12:33 +0100

    Treesitter bump to main branch, need:         - neovim >= 0.12 (or nightly)         - treesitter-cli >= 0.26

--- cfmhome/noarch/.config/nvim/lua/ritrac/plugins/treesitter.lua
+++ cfmhome/noarch/.config/nvim/lua/ritrac/plugins/treesitter.lua
@@ -4,32 +4,37 @@ return {
     enabled = true,
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    branch = "master",
+    branch = "main",
     lazy = false,
 
-    config = function ()
-        require'nvim-treesitter.configs'.setup {
+    config = function()
+        local parsers = {
+            "c", "cpp", "css", "go", "asm", "bash", "cuda", "diff",
+            "gitcommit", "gitignore", "gowork", "json", "cmake",
+            "make", "perl", "pod", "python", "php", "rst", "rust",
+            "ssh_config", "tmux", "xml", "yaml", "zig", "go", "css",
+            "lua", "vim", "vimdoc", "query", "elixir", "regex",
+            "heex", "javascript", "html", "markdown", "markdown_inline" }
 
-            ensure_installed = {
-                "c", "cpp", "css", "go", "asm", "bash", "cuda", "diff",
-                "gitcommit", "gitignore", "gowork", "json", "cmake",
-                "make", "perl", "pod", "python", "php", "rst", "rust",
-                "ssh_config", "tmux", "xml", "yaml", "zig", "go", "css",
-                "lua", "vim", "vimdoc", "query", "elixir", "regex",
-                "heex", "javascript", "html", "markdown", "markdown_inline" },
-            sync_install = false,
-            auto_install = true,
-            highlight = { enable = true
-                -- disable = { "c", "rust" },
-            },
-            indent = { enable = true },
+        require('nvim-treesitter').install(parsers)
 
-            incremental_selection = { enable = true },
-            textobjects = { enable = true },
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function(args)
+                local buf, filetype = args.buf, args.match
+                local language = vim.treesitter.language.get_lang(filetype)
+                if not language then return end
 
-        }
+                -- check if parser exists and load it
+                if not vim.treesitter.language.add(language) then return end
+                -- enables syntax highlighting and other treesitter features
+                vim.treesitter.start(buf, language)
+
+                -- enables treesitter based folds
+                -- for more info on folds see `:help folds`-- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                -- vim.wo.foldmethod = 'expr'
+
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+            end,
+        })
     end
 }
-
-
-
