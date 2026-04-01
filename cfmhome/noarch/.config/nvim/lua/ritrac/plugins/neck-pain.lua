109cff16d39a8da1067bf9f1c30d3f7719d04d20
Clément Coussirat <174454106+ritrac@users.noreply.github.com>
2026-04-01 14:37:53 +0200

    NoNeckPain tuning

--- cfmhome/noarch/.config/nvim/lua/ritrac/plugins/neck-pain.lua
+++ cfmhome/noarch/.config/nvim/lua/ritrac/plugins/neck-pain.lua
@@ -4,9 +4,16 @@ return {
     enabled = true,
     config = function()
         require('no-neck-pain').setup({
-            -- width = 120,
+            width = 90, -- ~80-85 usable columns
             mappings = {
                 enabled = true,
+                -- toggle = "<Leader>np"
+                -- toggleLeftSide = "<Leader>nql"
+                -- toggleRightSide = "<Leader>nqr"
+                -- widthUp = "<Leader>n="
+                widthUp = "<Leader>n+"
+                -- widthDown = "<Leader>n-"
+                -- scratchPad = "<Leader>ns"
             },
             buffers = {
                 disable_scroll = true,
@@ -14,6 +21,11 @@ return {
                     background = "#000000",
                     -- blend = -0.3,
                 }
+            },
+            autocmds = {
+                enableOnVimEnter = true,
+                enableOnTabEnter = true,
+                skipEnterNoNeckPainBuffer = true,
             },
         })
         require('no-neck-pain').enable()
